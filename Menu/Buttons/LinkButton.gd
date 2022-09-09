d2bdbe0696aae3f8ab93d6f2827d98e7867f774f
RepeatedKibbles <101306285+RepeatedKibbles@users.noreply.github.com>
2022-09-08 20:19:12 -0400

    Update

--- Menu/Buttons/LinkButton.gd
+++ Menu/Buttons/LinkButton.gd
@@ -1,4 +1,4 @@
 extends LinkButton
 
 func _on_LinkButton_pressed():
-	OS.shell_open("https://creativecommons.org/licenses/by/4.0/")
+	OS.shell_open("https://github.com/RepeatedKibbles/Horror-FPS-framework/blob/main/LICENSE.md")
